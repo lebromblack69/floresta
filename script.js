5a6e2e00abcc9606d41f861d1de059af308aae7c
lebronblack69 <143099552+lebromblack69@users.noreply.github.com>
2024-08-13 16:38:25 -0300

    criou as perguntas

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal=document.querySelector(".caixa-pricipal");
+const caixaPerguntas=document.querySelector("caixa-pergunta");
+const caixaAlternativas=document.querySelector("caixa-alternativa");
+const caixaResultado=document.querySelector("caixa-resultado");
+const caixaResultado=document.querySelector("caixa-resultado");
+    
+const perguntas=[
+    {
+      enunciado:"pergunta 1",
+      alternativa:[
+        "alternativa 1",
+        "alternativa 2"
+      ]
+},
+{
+    enunciado:"pergunta 2",
+    alternativa:[
+      "alternativa 1",
+      "alternativa 2"
+    ]
+}
+];
